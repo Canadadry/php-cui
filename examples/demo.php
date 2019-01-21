374e559212c773bd110476f602cae4dd0312620b
Mourey jerome <jerome@everycheck.fr>
2019-01-21 15:26:05 +0100

    start playing with tty

--- examples/demo.php
+++ examples/demo.php
@@ -4,5 +4,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Canadadry\PhpCui\Cui;
 
+system('stty cbreak - echo'); 
+$stdin = fopen('php://stdin', 'r');
+
 $cui = new Cui();
-echo $cui->helloWordl() . "\n";
+echo $cui->clear();
+echo $cui->move(100,10);
+while (1) {
+	$c = ord(fgetc($stdin));
+    echo $cui->clear();
+	echo $cui->move(10,10);
+    echo "Char read: $c\n";
+	echo $cui->move(100,10);
+}
--- src/Cui.php
+++ src/Cui.php
@@ -7,4 +7,14 @@ class Cui
 	{
 		return "hello world!";
 	}	
+
+	public function clear()
+	{
+		return "\033[2J";
+	}
+
+	public function move($x,$y)
+	{
+		return "\033[".$x.";".$y."H";
+	}
 };
